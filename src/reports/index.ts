a74e6e0e8bebabf285665b3b65ab7f4e79caf1e2
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-06-27 13:48:20 -0400

    feat: create index reports

--- src/reports/index.ts
+++ src/reports/index.ts
@@ -2,3 +2,7 @@ export * from './hello-world.report';
 export * from './employment-letter.report';
 export * from './employment-letter-by-id.report';
 export * from './sections/header.section';
+export * from './countries.report';
+export * from './sections/footer-total.section';
+export * from './sections/footer.section';
+export * from './sections/table-main.section';
